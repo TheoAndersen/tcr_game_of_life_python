0e6539717323f34e8d8dfd6287fd1df8866e2d07
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-15 23:28:15 +0200

    TCR , cells elf.cells = cell [(2, 2)]

--- test_gol.py
+++ test_gol.py
@@ -1,10 +1,10 @@
 import unittest
 
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, cells):
+        self.cells = cells
 
 class TestThis(unittest.TestCase):
     def test_should_be_able_to_create_a_game(self):
-        game = Game()
+        game = Game([(2, 2)])
 
