fcd7acdb43641db789824065739f27f22c43473a
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-15 23:34:41 +0200

    TCR def number_neighbors(cells): return 1 def test_nei

--- test_gol.py
+++ test_gol.py
@@ -3,10 +3,14 @@ import unittest
 class Game:
     def __init__(self, cells):
         self.cells = cells
+def number_neighbors(cells):
+    return 1
 
 class TestThis(unittest.TestCase):
     def test_should_be_able_to_create_a_game(self):
         game = Game([(2, 2)])
         self.assertEqual(1, len(game.cells))
 
+    def test_neighboars(self):
+        self.assertEqual(1, number_neighbors([(2, 2)]))
 
