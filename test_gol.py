ed3eca3d2f2c8ee278b14373da61564ca80209f2
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-15 23:27:48 +0200

    TCR class Game: def __init__(self): pass g m = G me

--- test_gol.py
+++ test_gol.py
@@ -1,6 +1,10 @@
 import unittest
 
+class Game:
+    def __init__(self):
+        pass
+
 class TestThis(unittest.TestCase):
     def test_(self):
-        self.assertEqual(1, 1)
+        game = Game()
 
