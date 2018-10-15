ecec1b006b22df152d84580ce00afa0c4f827d22
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-15 23:37:56 +0200

    TCR , cell (2, 2), (2, 2), (2, 2), [(2, 1), (2, 2), (2

--- test_gol.py
+++ test_gol.py
@@ -4,7 +4,7 @@ class Game:
     def __init__(self, cells):
         self.cells = cells
         
-def number_neighbors(cells):
+def number_neighbors(cell, cells):
     return len(cells) - 1
 
 class TestThis(unittest.TestCase):
@@ -13,7 +13,8 @@ class TestThis(unittest.TestCase):
         self.assertEqual(1, len(game.cells))
 
     def test_neighboars(self):
-        self.assertEqual(0, number_neighbors([(2, 2)]))
-        self.assertEqual(1, number_neighbors([(2, 1), (2, 2)]))
-        self.assertEqual(2, number_neighbors([(2, 1), (2, 2), (2, 3)]))
+        self.assertEqual(0, number_neighbors((2, 2), [(2, 2)]))
+        self.assertEqual(1, number_neighbors((2, 2), [(2, 1), (2, 2)]))
+        self.assertEqual(2, number_neighbors((2, 2), [(2, 1), (2, 2), (2, 3)]))
+        self.assertEqual(2, number_neighbors((2, 2), [(2, 1), (2, 2), (2, 3)]))
 
