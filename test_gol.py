02d13cdd0a3bc34bcb03eae31c3533663cc3ab79
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-15 23:35:15 +0200

    TCR 0 0

--- test_gol.py
+++ test_gol.py
@@ -5,7 +5,7 @@ class Game:
         self.cells = cells
         
 def number_neighbors(cells):
-    return 1
+    return 0
 
 class TestThis(unittest.TestCase):
     def test_should_be_able_to_create_a_game(self):
@@ -13,5 +13,5 @@ class TestThis(unittest.TestCase):
         self.assertEqual(1, len(game.cells))
 
     def test_neighboars(self):
-        self.assertEqual(1, number_neighbors([(2, 2)]))
+        self.assertEqual(0, number_neighbors([(2, 2)]))
 
