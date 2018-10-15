7ceee0f02a20282c8377fa9359feb2c104b7b0e1
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-15 23:41:55 +0200

    TCR neiboars = 0 for (x, y) in cells: if(x-1 >= cx and

--- test_gol.py
+++ test_gol.py
@@ -6,6 +6,11 @@ class Game:
         
 def number_neighbors(cell, cells):
     (cx, cy) = cell
+    neiboars = 0
+    for (x, y) in cells:
+        if(x-1 >= cx and x+1 <= cx and y-1 >= cy and y+1 <= cy):
+            neiboars = neiboars + 1
+        
     return len(cells) - 1
 
 class TestThis(unittest.TestCase):
