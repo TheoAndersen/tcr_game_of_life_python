94efc953b3e2ab7efc0a89ee3ebc495f1e77c79b
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-16 18:28:02 +0200

    TCR self.assertIn((2, 1), get_neighbours((1, 1), cells

--- test_gol.py
+++ test_gol.py
@@ -53,3 +53,4 @@ class TestThis(unittest.TestCase):
 
         self.assertIn((1, 2), get_neighbours((1, 1), cells))
         self.assertNotIn((3, 2), get_neighbours((1, 1), cells))
+        self.assertIn((2, 1), get_neighbours((1, 1), cells))
