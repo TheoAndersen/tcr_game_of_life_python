2813a4c21a30b48038b86ca0e28d5bcf2fe59a22
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-16 18:09:13 +0200

    TCR # -c- # cXc # -c-

--- test_gol.py
+++ test_gol.py
@@ -16,5 +16,8 @@ class TestThis(unittest.TestCase):
     def test_neighboars(self):
         cell = (2, 2)
         cells = [(1, 2), (3, 2), (2, 1), (2, 2), (2, 3)]
+        # -c-
+        # cXc
+        # -c-
         self.assertEqual(4, number_neighbors(cell, cells))
 
