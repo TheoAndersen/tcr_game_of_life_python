eec3f82b74c080a67e4b37633ab1364315ce2431
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-16 18:35:21 +0200

    TCR le (g t u ) le (g t u )

--- test_gol.py
+++ test_gol.py
@@ -17,17 +17,6 @@ def get_neighbours(cell, cells):
     return neighbours
 
 
-def number_neighbors(cell, cells):
-    (cx, cy) = cell
-    n = 0
-    for (x, y) in cells:
-        if cx - 1 <= x and cx + 1 >= x and cy - 1 <= y and cy + 1 >= y:
-            if not (cy == y and cx == x):
-                n = n + 1
-
-    return n
-
-
 class TestThis(unittest.TestCase):
     def test_should_be_able_to_create_a_game(self):
         game = Game([(2, 2)])
@@ -38,12 +27,12 @@ class TestThis(unittest.TestCase):
         # -c-
         # cXc
         # -c-
-        self.assertEqual(4, number_neighbors((2, 2), cells))
+        self.assertEqual(4, len(get_neighbours((2, 2), cells)))
 
         # -c-
         # Xcc
         # -c-
-        self.assertEqual(3, number_neighbors((1, 2), cells))
+        self.assertEqual(3, len(get_neighbours((1, 2), cells)))
 
         self.assertIn((2, 1), get_neighbours((2, 2), cells))
         self.assertIn((1, 2), get_neighbours((2, 2), cells))
