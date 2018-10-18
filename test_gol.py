cc3857bb5a9973388b73456c2ac669997ad9823c
Theo Andersen Carton <andersen.theo@gmail.com>
2018-10-18 09:24:39 +0200

    TCR get_grid bounds get grid_bounds

--- test_gol.py
+++ test_gol.py
@@ -29,7 +29,7 @@ class Game:
         self.cells = next_step_cells
         return self
 
-    def max_x(self):
+    def get_grid_bounds(self):
         x = max(map(lambda cell: cell.x, self.cells))
         return x
 
@@ -46,7 +46,7 @@ class Cell:
 class TestThis(unittest.TestCase):
     def return_max_x_based_on_current_cells(self):
         game = Game([Cell(1, 1), Cell(1, 3), Cell(2, 2), Cell(3, 1), Cell(3, 3)])
-        self.assertEqual(3, game.max_x())
+        self.assertEqual(3, game.get_grid_bounds())
 
     def test_step_more_than_three_neighbours_kills_the_cell(self):
         # c-c
